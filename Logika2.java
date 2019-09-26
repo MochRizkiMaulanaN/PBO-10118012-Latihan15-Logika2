6b9ff614d4cf13570dba8873525b773981978e98
MochRizkiMaulanaN <55683135+MochRizkiMaulanaN@users.noreply.github.com>
2019-09-26 15:11:57 +0700

    Add files via upload

---
+++ Logika2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logika2;
+
+/**
+ * Nama     : Moch Rizki Maulana N
+ * Kelas    : IF-1 2018
+ * Nim      : 10118012
+ * Deskripsi Program : Logika 2
+ *
+ * @author LENOVO
+ */
+public class Logika2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = "+(a && b));
+        System.out.println("a || b = "+(a || b));
+        System.out.println("!a = "+a);
+        System.out.println("!(a && b) = "+!(a && b));
+    }
+    
+}
